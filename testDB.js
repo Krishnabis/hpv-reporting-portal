1d054eba463dcc374061e434a8967d386d858f12
Krishna Bisht <krishnabisht1188@Gmail.com>
2026-09-03 18:03:53 +0530

    chore: remove vaccine stock management and ledger reporting

--- testDB.js
+++
@@ -1,6 +0,0 @@
-import { supabase } from './server/db/database.js';
-async function run() {
-  const { data } = await supabase.from('vaccine_stock_ledger').select('*').limit(5);
-  console.log(JSON.stringify(data, null, 2));
-}
-run();
